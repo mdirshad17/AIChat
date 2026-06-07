27d61bf5351df669fb621c5a5a0d5b94cf476973
mdirshad <irshadvmc@gmail.com>
2026-06-07 11:14:36 +0530

    added changes .sh file

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ set +a
 pkill -f 'app.jar' || true
 sleep 2
 
-nohup java -javaagent:~/newrelic.jar -jar ~/app.jar > ~/app.log 2>&1 < /dev/null &
+nohup java -javaagent:/home/ubuntu/newrelic.jar -jar /home/ubuntu/app.jar > /home/ubuntu/app.log 2>&1 < /dev/null &
 disown $!
