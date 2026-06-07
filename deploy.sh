db4bc8c25b2ffdd45d11e7138332aee054643ffb
mdirshad <irshadvmc@gmail.com>
2026-06-07 17:03:38 +0530

    added changes for new relic

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export $(grep -v '^#' ~/.env | xargs)
 
-sudo systemctl restart app
+pkill -f 'app.jar' || true
+sleep 2
+
+nohup java \
+  -javaagent:/home/ubuntu/newrelic.jar \
+  -Dnewrelic.config.license_key=$NEW_RELIC_KEY \
+  -Dnewrelic.config.app_name=AI_CHATBOT \
+  -jar /home/ubuntu/app.jar > /home/ubuntu/app.log 2>&1 < /dev/null &
+disown $!
