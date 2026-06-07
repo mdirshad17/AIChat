7ab9c74bda222da85a54828bb405ad9860c5af6b
mdirshad <irshadvmc@gmail.com>
2026-06-07 11:36:13 +0530

    added new changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -a
-source ~/.env
-set +a
+export $(grep -v '^#' ~/.env | xargs)
 
-pkill -f 'app.jar' || true
-sleep 2
-
-nohup java -javaagent:/home/ubuntu/newrelic.jar -jar /home/ubuntu/app.jar > /home/ubuntu/app.log 2>&1 < /dev/null &
-disown $!
+sudo systemctl restart app
